9402b73f72675fa64a849608902d71605ec4cac2
tushar <tusharturkane29@gmail.com>
2022-02-27 11:18:48 +0530

    space remove

--- src/test/java/basePackage/Pojo.java
+++ src/test/java/basePackage/Pojo.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Pojo {
-	
-	//base class space added
-	
+public class Pojo {	
 	
 	public static WebDriver LaunchChrome()
 	{
